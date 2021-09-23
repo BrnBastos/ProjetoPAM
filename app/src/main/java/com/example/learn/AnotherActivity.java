722dae5cd09bc2fb5620fba685b8ed6e34ccdbf0
BrnBastos <bruninhosbastos0228@gmail.com>
2021-09-22 21:05:52 -0300

    shared feita com sucesso carreta furacão

---
+++ app/src/main/java/com/example/learn/AnotherActivity.java
@@ -0,0 +1,55 @@
+package com.example.learn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    TextView mNameTv, mAgeTv;
+    Button mLogoutBtn;
+    SharedPreferences preferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        mNameTv = findViewById(R.id.nameTv);
+        mAgeTv = findViewById(R.id.ageTv);
+        mLogoutBtn = findViewById(R.id.logout);
+
+        preferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE);
+
+        String name = preferences.getString("NAME", "");
+        mNameTv.setText(name);
+        int age = preferences.getInt("AGE", 0);
+        mAgeTv.setText(""+age);
+
+        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.apply();
+
+                Intent intent = new Intent(AnotherActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+    public void Tela_Menu(View view){
+
+        Intent intent = new Intent(getApplicationContext(), Menu_inicial.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/learn/MainActivity.java
+++ app/src/main/java/com/example/learn/MainActivity.java
@@ -12,15 +12,60 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mNameEt, mAgeEt;
+    CheckBox mRemember;
+    Button mBtn;
+    SharedPreferences sharedPreferences;
+    boolean isRemembered = false;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void Tela_Menu(View view){
+        mNameEt = findViewById(R.id.nameEt);
+        mAgeEt = findViewById(R.id.ageEt);
+        mRemember = findViewById(R.id.checkbox);
+        mBtn = findViewById(R.id.login);
 
-        Intent intent = new Intent(getApplicationContext(), Menu_inicial.class);
-        startActivity(intent);
+        sharedPreferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE);
+
+        isRemembered = sharedPreferences.getBoolean("CHECKBOX", false);
+
+        if(isRemembered){
+            Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        mBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                    String name = mNameEt.getText().toString();
+                    int age = Integer.parseInt(mAgeEt.getText().toString().trim());
+                    boolean checked = mRemember.isChecked();
+
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("NAME", name);
+                    editor.putInt("AGE", age);
+                    editor.putBoolean("CHECKED", checked);
+                    editor.apply();
+
+                    Toast.makeText(MainActivity.this, "Information Saved!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
+                    startActivity(intent);
+                    finish();
+            }
+
+        });
+
+
+
+
+
     }
 }
